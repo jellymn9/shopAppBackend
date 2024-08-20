67f9627aa6dc74ef78b986795341fbd02327f2b7
Jelena <jelena.mandzic@gmail.com>
2024-08-20 17:37:08 +0200

    Add new products
    Extand categories with CREAM enum

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -8,31 +8,36 @@ async function main() {
   const dummyPassword = "asdf_asdf";
   const hashedPassword = await bcrypt.hash(dummyPassword, 10);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const alice = await prisma.users.upsert({
-    where: { email: "jeca@jeca.com" },
-    update: {},
-    create: {
-      id: "3bf9e7e0-1d14-4b6e-a1b8-a38f215e8d05",
-      email: "alice@prisma.io",
-      username: "Alice",
-      password: hashedPassword,
-    },
-  });
+  // const alice = await prisma.users.upsert({
+  //   where: { email: "jeca@jeca.com" },
+  //   update: {},
+  //   create: {
+  //     id: "3bf9e7e0-1d14-4b6e-a1b8-a38f215e8d05",
+  //     email: "alice@prisma.io",
+  //     username: "Alice",
+  //     password: hashedPassword,
+  //   },
+  // });
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const bob = await prisma.users.upsert({
-    where: { email: "bob@bob.io" },
-    update: {},
-    create: {
-      id: "137e03be-59d0-4805-a15c-44b5f4c1b3ce",
-      email: "bob@prisma.io",
-      username: "Bob",
-      password: hashedPassword,
-    },
-  });
+  // const bob = await prisma.users.upsert({
+  //   where: { email: "bob@bob.io" },
+  //   update: {},
+  //   create: {
+  //     id: "137e03be-59d0-4805-a15c-44b5f4c1b3ce",
+  //     email: "bob@prisma.io",
+  //     username: "Bob",
+  //     password: hashedPassword,
+  //   },
+  // });
   //categories
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const categories = await prisma.categories.createMany({
-    data: [{ name: "FLORAL_WATER" }, { name: "OIL" }, { name: "SOAP" }],
+    data: [
+      { name: "FLORAL_WATER" },
+      { name: "OIL" },
+      { name: "SOAP" },
+      { name: "CREAM" },
+    ],
     skipDuplicates: true,
   });
   //products
@@ -51,6 +56,52 @@ async function main() {
         },
       },
     },
+  });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const manyProducts = await prisma.products.createMany({
+    data: [
+      {
+        id: "011fda23-ea15-4543-9a13-6b932c8c0676",
+        name: "lavander hair shampoo",
+        price: "45$",
+        image: "image_123",
+        categoryName: "SOAP",
+        productTypeName: "SOLID_SHAMPOOS_AND_HAIR_SOAPS",
+      },
+      {
+        id: "ae825c6e-daa8-4568-9117-b9473fa01f7d",
+        name: "Pear body scrub",
+        price: "45$",
+        image: "image_123",
+        categoryName: "SOAP",
+        productTypeName: "SCRUBS_AND_MASKS",
+      },
+      {
+        id: "08118746-82a3-43bc-9339-d13a1599dae7",
+        name: "Pear hair mask",
+        price: "45$",
+        image: "image_123",
+        categoryName: "SOAP",
+        productTypeName: "SCRUBS_AND_MASKS",
+      },
+      {
+        id: "ce88cfed-0343-4206-8d64-68b43059e209",
+        name: "Pine deodorant",
+        price: "45$",
+        image: "image_123",
+        categoryName: "SOAP",
+        productTypeName: "NATURAL_DEODORANTS",
+      },
+      {
+        id: "36ae2d93-9cfc-4fc1-ab33-4fc1eb0ad0ee",
+        name: "acacia scent",
+        price: "83$",
+        image: "image_123",
+        categoryName: "FLORAL_WATER",
+        productTypeName: "EAU_DE_TOILETE",
+      },
+    ],
+    skipDuplicates: true,
   });
   //ProductTypes
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
