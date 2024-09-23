72e19626871a8981f27e07f5a409c63cc2a8f9bd
Jelena <jelena.mandzic@gmail.com>
2024-09-23 17:53:46 +0200

    Enable all cors requests

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 //import { boolParser } from "express-query-boolean";
 
 import mainErrorMiddleware from "./middlewares/errorMiddleware";
@@ -7,6 +8,8 @@ import routes from "./routes/routes";
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 //app.use(boolParser());
