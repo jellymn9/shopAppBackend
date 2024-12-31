7a20c1732df67adb661117f9e7ee3f7f02075384
Jelena <jelena.mandzic@gmail.com>
2024-12-31 21:39:09 +0100

    Add new migration to remove price column

--- src/prisma/seeds/seed.ts
+++ src/prisma/seeds/seed.ts
@@ -88,142 +88,157 @@ async function main() {
       {
         id: "011fda23-ea15-4543-9a13-6b932c8c0676",
         name: "lavander hair shampoo",
-        price: "45$",
+        //price: "45$",
         image: "image_123",
         categoryName: "SOAP",
         productTypeName: "SOLID_SHAMPOOS_AND_HAIR_SOAPS",
+        priceInt: 45,
       },
       {
         id: "ae825c6e-daa8-4568-9117-b9473fa01f7d",
         name: "Pear body scrub",
-        price: "45$",
+        //price: "45$",
         image: "image_123",
         categoryName: "SOAP",
         productTypeName: "SCRUBS_AND_MASKS",
+        priceInt: 45,
       },
       {
         id: "08118746-82a3-43bc-9339-d13a1599dae7",
         name: "Pear hair mask",
-        price: "45$",
+        //price: "45$",
         image: "image_123",
         categoryName: "SOAP",
         productTypeName: "SCRUBS_AND_MASKS",
+        priceInt: 45,
       },
       {
         id: "ce88cfed-0343-4206-8d64-68b43059e209",
         name: "Pine deodorant",
-        price: "45$",
+        //price: "45$",
         image: "image_123",
         categoryName: "SOAP",
         productTypeName: "NATURAL_DEODORANTS",
+        priceInt: 45,
       },
       {
         id: "36ae2d93-9cfc-4fc1-ab33-4fc1eb0ad0ee",
         name: "acacia scent",
-        price: "83$",
+        //price: "83$",
         image: "image_123",
         categoryName: "FLORAL_WATER",
         productTypeName: "EAU_DE_TOILETE",
+        priceInt: 83,
       },
       {
         id: "0a5b82ed-0eba-4317-9b02-e685c2850887",
         name: "Lavender Bliss Oil",
-        price: "234$",
+        //price: "234$",
         image: "some_image",
         categoryName: "OIL",
         productTypeName: "HAIR_OILD_AND_SERUMS",
+        priceInt: 234,
       },
 
       {
         id: "693d7f2d-1e9c-4dca-befc-098fbb0c6ec9",
         name: "Rose Petal Glow Cream",
-        price: "200$",
+        //price: "200$",
         image: "some_image",
         categoryName: "CREAM",
         productTypeName: "SCRUBS_AND_MASKS",
+        priceInt: 200,
       },
       {
         id: "f8668301-8473-4184-a734-e6eb96545071",
         name: "Peppermint Fresh Floral Water",
-        price: "150$",
+        //price: "150$",
         image: "some_image",
         categoryName: "FLORAL_WATER",
         productTypeName: "EAU_DE_TOILETE",
+        priceInt: 150,
       },
       {
         id: "3f29e67b-0195-4297-a5a3-8a991d53df38",
         name: "Lemon Zest Cleansing Soap",
-        price: "120$",
+        //price: "120$",
         image: "some_image",
         categoryName: "SOAP",
         productTypeName: "SOLID_SHAMPOOS_AND_HAIR_SOAPS",
+        priceInt: 120,
       },
       {
         id: "a1bbb0b8-0bda-4ca4-923a-5b016f4e0268",
         name: "Eucalyptus Vitality Oil",
-        price: "180$",
+        //price: "180$",
         image: "some_image",
         categoryName: "OIL",
         productTypeName: "HAIR_OILD_AND_SERUMS",
+        priceInt: 180,
       },
       {
         id: "10e4086c-fcab-4112-a6b4-dd5d61cc47ec",
         name: "Orange Radiance Cream",
-        price: "130$",
+        //price: "130$",
         image: "some_image",
         categoryName: "CREAM",
         productTypeName: "SCRUBS_AND_MASKS",
+        priceInt: 130,
       },
       {
         id: "65504f26-7f38-4f7b-a889-8d00b4f3c55f",
         name: "Tea Tree Revitalizing Soap",
-        price: "140$",
+        //price: "140$",
         image: "some_image",
         categoryName: "SOAP",
         productTypeName: "SOLID_SHAMPOOS_AND_HAIR_SOAPS",
+        priceInt: 140,
       },
       {
         id: "d07ecf1a-20f3-49db-a1ad-a61023c49379",
         name: "Chamomile Calm Floral Water",
-        price: "190$",
+        //price: "190$",
         image: "some_image",
         categoryName: "FLORAL_WATER",
         productTypeName: "EAU_DE_TOILETE",
+        priceInt: 190,
       },
       {
         id: "7b275400-4835-477e-a6b6-d333411bfa4e",
         name: "Ylang Ylang Dream Oil",
-        price: "240$",
+        //price: "240$",
         image: "some_image",
         categoryName: "OIL",
         productTypeName: "EAU_DE_TOILETE",
+        priceInt: 240,
       },
       {
         id: "c0d5ef10-7722-4c73-a364-6db3cc3c59a6",
         name: "Wintergreen Harmony Soap",
-        price: "225$",
+        //price: "225$",
         image: "some_image",
         categoryName: "SOAP",
         productTypeName: "SOLID_SHAMPOOS_AND_HAIR_SOAPS",
+        priceInt: 225,
       },
     ],
     skipDuplicates: true,
   });
 
-  const products = await prisma.products.findMany();
+  //const products = await prisma.products.findMany();
 
-  for (const product of products) {
-    // Remove dollar sign and convert to number
-    const numericPrice = parseInt(product.price.replace("$", ""), 10);
+  // for (const product of products) {
+  //   // Remove dollar sign and convert to number
+  //   const numericPrice = parseInt(product.price.replace("$", ""), 10);
 
-    // Update the new priceInt column
-    await prisma.products.update({
-      where: { id: product.id },
-      data: { priceInt: numericPrice },
-    });
-  }
+  //   // Update the new priceInt column
+  //   await prisma.products.update({
+  //     where: { id: product.id },
+  //     data: { priceInt: numericPrice },
+  //   });
+  // }
 
-  console.log("Price data cleaned and migrated to priceInt column.");
+  // console.log("Price data cleaned and migrated to priceInt column.");
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const tagsOnProducts = await prisma.tagsOnProducts.createMany({
